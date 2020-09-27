4a0e06649fc8547c6425a133c332ce2c307b8c61
Darya Lapa <daryalapa89@gmail.com>
2020-09-27 23:42:48 +0300

    setup.py added

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='Hangman',
+    version='1.0.',
+    author='Darya Lapa',
+    author_email='daryalapa89@gmail.com',
+    url='https://github.com/daryalapa/Hangman',
+    license='MIT',
+    test_suite='tests',
+    tests_require=[
+        "unittest",
+    ],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ]
+)
